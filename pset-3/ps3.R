a66b4d3f568040f8bd21cced91aae94ff61d946f
brayanflorez <90009617+brayanflorez@users.noreply.github.com>
2024-04-25 11:21:31 -0500

    Punto 1

--- pset-3/ps3.R
+++ pset-3/ps3.R
@@ -16,4 +16,39 @@ rm(list=ls())
 ##Instalar y llamar librerías
 require(pacman)
 
-p_load(tidyverse, rio, skimr, janitor, haven) 
+p_load(tidyverse, rio, skimr, janitor, haven, data.table) 
+
+#############################################
+####1. BUCLES
+
+#1.1 Lista de archivos
+nombres_archivos <- list.files(path = "pset-3/input", full.names = TRUE, recursive = TRUE)
+print(nombres_archivos)
+
+#1.2 Importar archivos
+lista_dataframes <- list() 
+for (nombre_archivo in nombres_archivos) {
+  if (grepl("No ocupados", nombre_archivo)) {
+    No_ocupados <- data.frame(readRDS(nombre_archivo))
+  }
+  if (grepl("No ocupados", nombre_archivo)) {
+    Ocupados <- data.frame(readRDS(nombre_archivo))
+  }
+  if (grepl("No ocupados", nombre_archivo)) {
+    Fuerza_trabajo <- data.frame(readRDS(nombre_archivo))
+  }
+}
+
+lista_dataframes <- list(No_ocupados=No_ocupados,
+                         Ocupados = Ocupados, 
+                         Fuerza_trabajo = Fuerza_trabajo)
+print(lista_dataframes)
+
+#1.3
+
+data_frame_combinado <- rbindlist(lista_dataframes)
+data_frame_combinado <- as.data.frame(data_frame_combinado)
+
+
+#############################################
+####2. PREPARACIÓN
